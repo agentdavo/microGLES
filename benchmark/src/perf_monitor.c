91dd9d3399d851a0d438a794c7a36313476959c0
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-22 20:39:14 +0100

    Add GLX version and visual logging

--- benchmark/src/perf_monitor.c
+++ benchmark/src/perf_monitor.c
@@ -190,11 +190,45 @@ int main(int argc, char **argv)
 	}
 #ifdef HAVE_X11
 	X11Window *win = x11_window_create(1024, 768, "microGLES Perf");
+	if (!win) {
+		LOG_ERROR("x11_window_create failed. DISPLAY=%s",
+			  getenv("DISPLAY"));
+	} else {
+		LOG_INFO("x11_window_create succeeded");
+	}
+	Display *dpy = NULL;
 	GLXContext glx_ctx = NULL;
 	if (win) {
-		Display *dpy = x11_window_get_display(win);
-		glx_ctx = glXCreateContext(dpy, NULL, NULL, False);
-		glXMakeCurrent(dpy, (GLXDrawable)(uintptr_t)win, glx_ctx);
+		dpy = x11_window_get_display(win);
+		int glx_major = 0, glx_minor = 0;
+		if (!glXQueryVersion(dpy, &glx_major, &glx_minor)) {
+			LOG_ERROR("glXQueryVersion failed");
+		} else {
+			LOG_INFO("GLX %d.%d available", glx_major, glx_minor);
+		}
+		static int visual_attr[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24,
+					     GLX_DOUBLEBUFFER, None };
+		XVisualInfo *vi =
+			glXChooseVisual(dpy, DefaultScreen(dpy), visual_attr);
+		if (!vi) {
+			LOG_ERROR("glXChooseVisual failed");
+		} else {
+			LOG_INFO("glXChooseVisual succeeded");
+		}
+		glx_ctx = glXCreateContext(dpy, vi, NULL, True);
+		if (glx_ctx) {
+			LOG_INFO("glXCreateContext succeeded");
+		} else {
+			LOG_ERROR("glXCreateContext failed");
+		}
+		if (vi)
+			XFree(vi);
+		if (glx_ctx &&
+		    glXMakeCurrent(dpy, (GLXDrawable)(uintptr_t)win, glx_ctx)) {
+			LOG_INFO("glXMakeCurrent succeeded");
+		} else if (glx_ctx) {
+			LOG_ERROR("glXMakeCurrent failed");
+		}
 	}
 #else
 	X11Window *win = NULL;
@@ -243,7 +277,7 @@ int main(int argc, char **argv)
 			thread_pool_wait();
 #ifdef HAVE_X11
 			if (win)
-				glXSwapBuffers(NULL,
+				glXSwapBuffers(dpy,
 					       (GLXDrawable)(uintptr_t)win);
 #endif
 			if (frame_idx < 2) {
@@ -282,7 +316,8 @@ int main(int argc, char **argv)
 	}
 #ifdef HAVE_X11
 	if (glx_ctx) {
-		glXDestroyContext(NULL, glx_ctx);
+		glXMakeCurrent(dpy, None, NULL);
+		glXDestroyContext(dpy, glx_ctx);
 		glx_ctx = NULL;
 	}
 	if (win)
