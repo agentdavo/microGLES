d67c1a18afa9ecf84e879a90b4b8014e2ce77509
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-22 13:06:54 +0100

    Fix fixed-point helpers and cleanup

--- src/gl_utils.h
+++ src/gl_utils.h
@@ -10,6 +10,7 @@
 #include <GLES/gl.h> /* OpenGL ES 1.1 */
 #include <GLES/glext.h> /* For extension types */
 #include <stddef.h> /* For size_t */
+#include "fixed_point.h" /* Fixed-point helpers */
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,9 +19,6 @@ extern "C" {
 /* Memory tracking functions */
 void *tracked_malloc(size_t size);
 void tracked_free(void *ptr, size_t size);
-
-#define FIXED_TO_FLOAT(x) ((GLfloat)(x) / 65536.0f)
-#define FLOAT_TO_FIXED(x) ((GLfixed)((x) * 65536.0f))
 
 /* Utility function to validate framebuffer completeness */
 GLboolean ValidateFramebufferCompleteness(void);
