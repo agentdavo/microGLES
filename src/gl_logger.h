28fd877c2c6b23bc7c80aa48d9e95b685c858bc4
David Smith <44442387+agentdavo@users.noreply.github.com>
2025-06-23 13:44:25 +0100

    Add colored stage and frame logging

--- src/gl_logger.h
+++ src/gl_logger.h
@@ -31,12 +31,34 @@ typedef enum {
 int logger_init(const char *path, LogLevel level);
 void logger_shutdown(void);
 void LogMessage(LogLevel level, const char *format, ...);
+void logger_set_indent(unsigned level);
 
 #define LOG_DEBUG(...) LogMessage(LOG_LEVEL_DEBUG, __VA_ARGS__)
 #define LOG_INFO(...) LogMessage(LOG_LEVEL_INFO, __VA_ARGS__)
 #define LOG_WARN(...) LogMessage(LOG_LEVEL_WARN, __VA_ARGS__)
 #define LOG_ERROR(...) LogMessage(LOG_LEVEL_ERROR, __VA_ARGS__)
 #define LOG_FATAL(...) LogMessage(LOG_LEVEL_FATAL, __VA_ARGS__)
+
+#ifndef NDEBUG
+#define ANSI_YELLOW "\x1b[33m"
+#define ANSI_GREEN "\x1b[32m"
+#define ANSI_RESET "\x1b[0m"
+#define LOG_FRAME_START(idx)                                                  \
+	do {                                                                  \
+		logger_set_indent(0);                                         \
+		LogMessage(LOG_LEVEL_INFO, ANSI_YELLOW "Frame %d" ANSI_RESET, \
+			   (idx));                                            \
+	} while (0)
+#define LOG_STAGE_START(name)                                          \
+	do {                                                           \
+		LogMessage(LOG_LEVEL_INFO, ANSI_GREEN "%s" ANSI_RESET, \
+			   (name));                                    \
+		logger_set_indent(1);                                  \
+	} while (0)
+#else
+#define LOG_FRAME_START(idx) ((void)0)
+#define LOG_STAGE_START(name) ((void)0)
+#endif
 
 #ifdef __cplusplus
 }
